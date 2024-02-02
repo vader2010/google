73bbd137912f703f19876098570937f1bc685d44
Michael Cutsail <65092568+vader2010@users.noreply.github.com>
2024-02-02 08:53:58 -0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://mathsspot.com";//your website url
+  let target = "https://websitesball.com/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
